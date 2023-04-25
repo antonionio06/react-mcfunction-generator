fc5e554394c93e81bb264dbfb7cebd17bae6590a
antonionio06 <antonionio2006@o2.pl>
2023-04-25 15:12:17 +0200

    added copy button and extended CommandsView class

--- src/components/commands_view.jsx
+++ src/components/commands_view.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import "../style.css";
 class CommandsView extends Component {
   state = {};
+  copyToClipboard = () => {
+    navigator.clipboard.writeText(this.props.content);
+  };
   render() {
-    if (this.props.content) {
-      return <pre>{this.props.content}</pre>;
-    } else {
+    if (!this.props.content) {
       return;
     }
+    let ncommands = this.props.content.split("\n").length - 1;
+    return (
+      <div className="layer1">
+        <button onClick={this.copyToClipboard}>Copy</button>
+        <p>{ncommands + " commands generated"}</p>
+        <textarea rows={50} cols={200}>
+          {this.props.content}
+        </textarea>
+      </div>
+    );
   }
 }
 
