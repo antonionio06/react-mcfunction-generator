e141395c30a7f665a2409d71cbf7a78ea616b3c1
antonionio06 <antonionio2006@o2.pl>
2023-04-28 20:12:23 +0200

    made the copy button turn green

--- src/components/commands_view.jsx
+++ src/components/commands_view.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import "../style.css";
 class CommandsView extends Component {
-  state = {};
+  state = {
+    copied: false,
+  };
   copyToClipboard = () => {
+    this.setState({ copied: true });
     navigator.clipboard.writeText(this.props.content);
   };
   render() {
     if (!this.props.content) {
       return;
     }
+    //resetting the state on render is probably an anti-pattern, but i'm too lazy to implement it other way.
+    let copied = this.state.copied;
+    this.state.copied = false;
     let ncommands = this.props.content.split("\n").length - 1;
     return (
       <div className="layer1">
-        <button onClick={this.copyToClipboard}>Copy</button>
+        <button
+          onClick={this.copyToClipboard}
+          className={copied ? "active" : ""}
+        >
+          Copy
+        </button>
         <span>{ncommands + " commands generated"}</span>
         <br />
         <textarea
